f007fd91bf74992b52a4b1916db79a4cb1bd9abe
notblankz <aahanrandomg@gmail.com>
2025-04-02 19:12:26 +0530

    Switch to argument-driven file input

--- src/main.py
+++ src/main.py
@@ -1,12 +1,30 @@
 import ZapCore.torrent_parser as Parser
 import ZapCore.tracker_request as Request
+import argparse
 
 def main():
-    torrent_metadata = Parser.parse_torrent("../tests/Balatro.torrent")
-    Parser.log_metadata(torrent_metadata)
-    peer_id = Request.generate_id()
-    tracker_response = Request.get_peers(torrent_metadata, peer_id)
-    Request.log_tracker_response(tracker_response)
+
+    parser = argparse.ArgumentParser(
+        description="ZapTorrent - A CLI based torrent client",
+        usage="\n  zap --parse <path to .torrent file>"
+              "\n  zap --download <path to .torrent file>",
+        )
+
+    parser.add_argument("--verbose", "-v", action="store_true", help="Enable detailed logging")
+    parser.add_argument("--parse", "-p", metavar="<path to .torrent file>" ,type=str, help="Parse and display metadata of the given .torrent file, including file details and tracker URLs.")
+    parser.add_argument("--download", "-d", metavar="<path to .torrent file>", type=str, help="Start downloading files from the given .torrent file using the BitTorrent protocol")
+
+    args = parser.parse_args()
+
+    if args.download:
+        torrent_metadata = Parser.parse_torrent(args.download)
+        if args.verbose: Parser.log_metadata(torrent_metadata)
+        peer_id = Request.generate_id()
+        tracker_response = Request.get_peers(torrent_metadata, peer_id)
+        if args.verbose: Request.log_tracker_response(tracker_response)
+    elif args.parse:
+        torrent_metadata = Parser.parse_torrent(args.parse)
+        Parser.log_metadata(torrent_metadata)
 
 if __name__ == "__main__":
     main()
